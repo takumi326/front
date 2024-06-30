014a12c698d84b16824ecf5010036d24060cb976
Hasshi <hasshi@DESKTOP-AU9HMFS>
2024-06-30 17:36:45 +0900

    TOPページ作成

--- app/components/form/TopForm.tsx
+++ app/components/form/TopForm.tsx
@@ -56,11 +56,11 @@ export const TopForm: React.FC = () => {
 
   return (
     <>
-      <div className="text-center pt-16">
+      <div className="text-center pt-8">
         <p className="font-bold text-7xl">Task × Money</p>
       </div>
       <ul>
-        <li className="flex mx-5 mt-16 justify-center">
+        {/* <li className="flex mx-5 mt-16 justify-center">
           <div className="flex">
             <div className="pr-32">
               <Image
@@ -83,6 +83,65 @@ export const TopForm: React.FC = () => {
               />
             </div>
           </div>
+        </li> */}
+        <li className="flex mx-5 mt-16 justify-center">
+          <div className="service-intro-section pb-18">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
+              <div className="service-card p-6 bg-white shadow-lg rounded-lg min-w-xs min-w-80 max-w-xs mx-auto">
+                <div className="flex justify-center items-center">
+                  <h3 className="text-2xl font-semibold mb-2 text-center">
+                    タスク
+                  </h3>
+                  <div className="service-icon mb-4">
+                    <Image
+                      src={taskImage}
+                      alt="LOGO"
+                      className="w-16 h-16 mx-auto"
+                    />
+                  </div>
+                  <h3 className="text-2xl font-semibold mb-2 text-center">
+                    管理
+                  </h3>
+                </div>
+                <p className="text-left text-lg pl-6 pb-3">
+                  目標を設定した後、その目標に対してのタスクを設定してスケジュールを立てることができます。
+                </p>
+                <p className="text-left text-lg pl-8 pb-3">
+                  ・目標ごとにタスクを選択
+                </p>
+                <p className="text-left text-lg pl-8 pb-3">・リピート機能</p>
+                <p className="text-left text-lg pl-8 pb-3">・カレンダー機能</p>
+              </div>
+              <div className="service-card p-6 bg-white shadow-lg rounded-lg min-w-xs min-w-80 max-w-xs mx-auto">
+                <div className="flex justify-center items-center">
+                  <h3 className="text-2xl font-semibold mb-2 text-center">
+                    お金
+                  </h3>
+                  <div className="service-icon mb-4">
+                    <Image
+                      src={moneyImage}
+                      alt="LOGO"
+                      className="w-16 h-16 mx-auto"
+                    />
+                  </div>
+                  <h3 className="text-2xl font-semibold mb-2 text-center">
+                    管理
+                  </h3>
+                </div>
+                <p className="text-left pl-6 pb-3">
+                  残高に口座の金額を入れて、そこから毎日支出、収入によって残高の金額が変動するようになっています。
+                </p>
+                <p className="text-left text-lg pl-8 pb-3">
+                  ・支出、収入、残高
+                </p>
+                <p className="text-left text-lg pl-8 pb-3">・リピート機能</p>
+                <p className="text-left text-lg pl-8 pb-3">
+                  ・予想残高確認機能
+                </p>
+                <p className="text-left text-lg pl-8 pb-3">・カレンダー機能</p>
+              </div>
+            </div>
+          </div>
         </li>
         {authenticated ? (
           <li className="pt-14 pb-20 "></li>
@@ -93,7 +152,7 @@ export const TopForm: React.FC = () => {
               className="font-bold text-xl bg-blue-500 px-3 rounded-full text-white mt-8 mb-9"
               onClick={handleSubmit}
             >
-              ゲストログイン
+              ゲストログインはこちら
             </button>
           </li>
         )}
